e84da8c9a951a2def216f9576eb6fbda3b8fc458
Jepifa <69280975+Jepifa@users.noreply.github.com>
2021-04-25 20:45:35 +0200

    Update DrawingView.kt

--- DrawingView.kt
+++ DrawingView.kt
@@ -69,3 +69,78 @@ class DrawingView @JvmOverloads constructor (context: Context, attributes: Attri
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {}
 }
+
+
+-----------------------------------------------------------------------------------------------------
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class DrawingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr), SurfaceHolder.Callback, Runnable {
+    lateinit var canvas: Canvas
+    val backgroundPaint = Paint()
+    var screenWidth = 0f
+    var screenHeight = 0f
+    var drawing = false
+    val echiquier = Echiquier(0f,0f,0f, 0f,this)
+    val cavalier = Cavalier(this)
+    val roi = Roi(this)
+    val tour = Tour(this)
+    lateinit var thread: Thread
+    init {
+        backgroundPaint.color = Color.WHITE
+    }
+
+    fun pause() {
+        drawing = false
+        thread.join()
+    }
+
+    fun resume() {
+        drawing = true
+        thread = Thread(this)
+        thread.start()
+    }
+
+    override fun run() {
+        while (drawing) {
+            draw()
+        }
+    }
+
+    override fun onSizeChanged(w:Int, h:Int, oldw:Int, oldh:Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        screenWidth = w.toFloat()
+        screenHeight = h.toFloat()
+        echiquier.ecart = (w / 6f)
+        echiquier.Xi= (w / 2f)
+        echiquier.Yi = (h / 8f)
+        echiquier.cibleFin = (h * 7 / 8f)
+        echiquier.setRect()
+    }
+
+    fun draw() {
+        if (holder.surface.isValid) {
+            canvas = holder.lockCanvas()
+            canvas.drawRect(0f, 0f, canvas.width.toFloat(),
+                canvas.height.toFloat(), backgroundPaint)
+            echiquier.draw(canvas)
+            /**cavalier.draw(canvas)
+            tour.draw(canvas)
+            roi.draw(canvas)**/
+            holder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int,
+                                width: Int, height: Int) {}
+
+    override fun surfaceCreated(holder: SurfaceHolder) {}
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {}
+}
