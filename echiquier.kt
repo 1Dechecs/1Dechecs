5b7d50d119441b159842f3f0aacaf880baff2c0a
lou-ver <83176860+lou-ver@users.noreply.github.com>
2021-04-25 20:10:26 +0200

    Update echiquier.kt

--- echiquier.kt
+++ echiquier.kt
@@ -32,3 +32,37 @@ class Echiquier (var cibleDistance: Float, var cibleDebut: Float, var cibleFin: 
         longueurPiece = (cibleFin - cibleDebut) / CIBLE_PIECES
     }
 }
+
+
+------------------------------------------------------------------------------------------------------------------------
+
+package com.example.echecier
+
+import android.graphics.*
+import android.view.SurfaceView
+
+class case (var Xi: Float, var Yi: Float, var ecart : Float) {
+
+    val nb_case = 8
+    val casePaint = Paint()
+    val nbPaint = Paint()
+    val contPaint = Paint()
+
+    fun draw(canvas: Canvas) {
+        nbPaint.color = Color.WHITE
+        contPaint.color = Color.BLACK
+        var X = Xi
+        var Y = Yi
+        val bordure = ecart / 8
+        canvas.drawRect(Xi - bordure, Yi - bordure, Xi + ecart + bordure, Yi + (8 * ecart) + bordure, contPaint)
+        for (i in 0 until nb_case) {
+            if (i % 2 != 0)
+                casePaint.color = Color.BLUE
+            else
+                casePaint.color = Color.GRAY
+            canvas.drawRect(X,Y,X + ecart, Y + ecart,casePaint)
+            canvas.drawText(i.toString(), X, Y, nbPaint)
+        }
+        Y += ecart
+    }
+}
