fcf21bec3e0cff23920b4d528c70d1a87958b677
Jepifa <69280975+Jepifa@users.noreply.github.com>
2021-04-27 14:35:13 +0200

    Update Pieces.kt

--- Pieces.kt
+++ Pieces.kt
@@ -1,4 +1,20 @@
 package com.example.1Dechecs
 
-class Pieces {
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+
+
+class Pièce (var pieceSize: Float, var view: DrawingView) {
+
+
+    val pieces = intArrayOf(R.drawable.roi_blanc,R.drawable.roi_noir,R.drawable.cavalier_blanc,R.drawable.cavalier_noir,R.drawable.tour_blanc,R.drawable.tour_noir)
+
+    fun onDraw(canvas: Canvas) {
+        val paint = Paint()
+        val roiBlancBitmap = BitmapFactory.decodeResource(drawable,pieces[1])
+        canvas?.drawBitmap(roiBlancBitmap, null, Rect(0,0,600,600), paint)
+    }
 }
+
