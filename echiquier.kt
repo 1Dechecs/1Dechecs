94349e5202a76f03c336bb0a0e5dc9bc981f3376
Jepifa <69280975+Jepifa@users.noreply.github.com>
2021-04-27 09:28:15 +0200

    Update echiquier.kt

--- echiquier.kt
+++ echiquier.kt
@@ -1,10 +1,10 @@
 import android.graphics.*
 
-class Echiquier (var cibleDistance: Float, var cibleDebut: Float, var cibleFin: Float, var width: Float, var view: DrawingView) {
+class Echiquier (var Xi: Float, var Yi: Float, var ecart: Float, var width: Float, var view: DrawingView) {
 
     val CIBLE_PIECES = 8
-    val cible = RectF(cibleDistance, cibleDebut,
-            cibleDistance + width, cibleFin)
+    val cible = RectF(Xi, Yi,
+            Xi + width, ecart)
     var cibleTouchee = BooleanArray(CIBLE_PIECES)
     val ciblePaint = Paint()
     var longueurPiece = 0f
@@ -27,9 +27,9 @@ class Echiquier (var cibleDistance: Float, var cibleDebut: Float, var cibleFin: 
     }
 
     fun setRect() {
-        cible.set(cibleDistance, cibleDebut,
-                cibleDistance + width, cibleFin)
-        longueurPiece = (cibleFin - cibleDebut) / CIBLE_PIECES
+        cible.set(Xi, Yi,
+                Xi + width, ecart)
+        longueurPiece = (ecart - Yi) / CIBLE_PIECES
     }
 }
 
