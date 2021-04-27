9a93da0974c4683a11e5d032638dd4c44b593f5d
Jepifa <69280975+Jepifa@users.noreply.github.com>
2021-04-27 09:32:13 +0200

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,3 +6,31 @@ import android.view.SurfaceView
 
 class Pieces @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr) {
 }
+
+---------------------------------------------------------------------------------------------------------------------
+
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity: AppCompatActivity() {
+
+    lateinit var DrawingView: DrawingView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        DrawingView = findViewById<DrawingView>(R.id.vMain)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        DrawingView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        DrawingView.resume()
+    }
+}
