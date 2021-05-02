95922d32579322f267f0527fcc2d986b1cb1d00e
loicnanga <83168471+loicnanga@users.noreply.github.com>
2021-05-02 18:52:05 +0200

    Update mouv.kt

--- mouv.kt
+++ mouv.kt
@@ -1,25 +1,25 @@
 fun main(val d : Case, var a : Case) {    //Case d = de départ, case a = d'arrivée
   if d.piece == Roi_Blanc     //Mouvement du roi
-    if Int(d) == Int(a+1) || Int(d) == Int(a-1)
+    if d == a+1 || d == a-1
       a.piece = Roi_Blanc
       d.piece = null
     else
       main()
   if d.piece == Cavalier_Blanc    //Mouvement du cavalier
-    if Int(d) == Int(a+2) || Int(d) == Int(a-2)
+    if d == a+2 || d == a-2
       a.piece = Cavalier_Blanc
       d.piece = null
     else
       main()
   if d.piece == Toure_Blanc   //Mouvement de la toure
     var t = true
-    for j in [Int(a), Int(d)]   //Cases intermédiaires vides
+    for (j in d until a)   //Cases intermédiaires vides
       if j.piece != null
         t = false
-      if t
+    if t
         a.piece = Toure_Blanc
     		d.piece = null
-    	else
+    else
     		main()
   else
     	main()
